4a6936d71137c66f715f51015c6bf64c206604c7
Valentino Garbagnoli <vgarbagnoli@mi.unc.edu.ar>
2026-03-12 21:37:45 -0300

    fix: solo hace suma

--- calculadora.cpp
+++ calculadora.cpp
@@ -6,33 +6,11 @@ int main() {
     char op;
     cout << "Enter first number: ";
     cin >> num1;
-    cout << "Enter operator (+, -, *, /): ";
-    cin >> op;
     cout << "Enter second number: ";
     cin >> num2;
-
-    switch(op) {
-        case '+':
-            result = num1 + num2;
-            break;
-        case '-':
-            result = num1 - num2;
-            break;
-        case '*':
-            result = num1 * num2;
-            break;
-        case '/':
-            if(num2 != 0)
-                result = num1 / num2;
-            else {
-                cout << "Error: Division by zero!" << endl;
-                return 1;
-            }
-            break;
-        default:
-            cout << "Invalid operator!" << endl;
-            return 1;
-    }
+    result =num1+num2;
+    
+    
     cout << "Result: " << result << endl;
     return 0;
 }
